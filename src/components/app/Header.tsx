a34cc42e5fb6382ede84b26d7dda9f57e42984b6
khai <ngvankhai0303@gmail.com>
2024-07-05 15:38:13 +0700

    upd

--- src/components/app/Header.tsx
+++ src/components/app/Header.tsx
@@ -31,78 +31,85 @@ export default function Header() {
     }
 
     return (
-        <div className="fixed top-0 left-0 w-full bg-black shadow-md z-50 px-4 md:px-48">
-            <div className="flex items-center justify-between h-24">
-                <div className="text-4xl font-bold">
-                    Van Khai<span className="text-green-600">.</span>
-                </div>
-                <div className="hidden md:flex items-center gap-x-6 font-semibold">
-                    <Link href="/" className={getLinkClassName("/")}>
-                        Home <span className={spanStyle("/")}></span>
-                    </Link>
-                    <Link
-                        href="/projects"
-                        className={getLinkClassName("/projects")}
-                    >
-                        Projects{" "}
-                        <span className={spanStyle("/projects")}></span>
-                    </Link>
-                    <Link
-                        href="/experience"
-                        className={getLinkClassName("/experience")}
-                    >
-                        Experience{" "}
-                        <span className={spanStyle("/experience")}></span>
-                    </Link>
-                    <Link
-                        href="/skills"
-                        className={getLinkClassName("/skills")}
-                    >
-                        Skills <span className={spanStyle("/skills")}></span>
-                    </Link>
-                    <Link
-                        href="/contact"
-                        className={getLinkClassName("/contact")}
-                    >
-                        Contact <span className={spanStyle("/contact")}></span>
-                    </Link>
-                </div>
-                <div className="md:hidden">
-                    <Dropdown>
-                        <DropdownTrigger>
-                            <Button variant="flat" color="success">
-                                Menu
-                            </Button>
-                        </DropdownTrigger>
-                        <DropdownMenu aria-label="Static Actions">
-                            <DropdownItem key="home">
-                                <Link href="/" className="w-full h-full">
-                                    Home
-                                </Link>
-                            </DropdownItem>
-                            <DropdownItem key="projects">
-                                <Link
-                                    href="/projects"
-                                    className="w-full h-full"
-                                >
-                                    Projects
-                                </Link>
-                            </DropdownItem>
-                            <DropdownItem key="experience">
-                                <Link
-                                    href="/experience"
-                                    className="w-full h-full"
-                                >
-                                    Experience
-                                </Link>
-                            </DropdownItem>
-                            <DropdownItem key="contact">
-                                <Link href="/contact" className="w-full h-full">
-                                    Contact
-                                </Link>
-                            </DropdownItem>
-                        </DropdownMenu>
-                    </Dropdown>
+        <div className="fixed top-0 left-0 w-full shadow-md z-50 bg-black">
+            <div className="px-4 md:px-48">
+                <div className="flex items-center justify-between h-24">
+                    <div className="text-4xl font-bold">
+                        Van Khai<span className="text-green-600">.</span>
+                    </div>
+                    <div className="hidden md:flex items-center gap-x-6 font-semibold">
+                        <Link href="/" className={getLinkClassName("/")}>
+                            Home <span className={spanStyle("/")}></span>
+                        </Link>
+                        <Link
+                            href="/projects"
+                            className={getLinkClassName("/projects")}
+                        >
+                            Projects{" "}
+                            <span className={spanStyle("/projects")}></span>
+                        </Link>
+                        <Link
+                            href="/experience"
+                            className={getLinkClassName("/experience")}
+                        >
+                            Experience{" "}
+                            <span className={spanStyle("/experience")}></span>
+                        </Link>
+                        <Link
+                            href="/skills"
+                            className={getLinkClassName("/skills")}
+                        >
+                            Skills{" "}
+                            <span className={spanStyle("/skills")}></span>
+                        </Link>
+                        <Link
+                            href="/contact"
+                            className={getLinkClassName("/contact")}
+                        >
+                            Contact{" "}
+                            <span className={spanStyle("/contact")}></span>
+                        </Link>
+                    </div>
+                    <div className="md:hidden">
+                        <Dropdown>
+                            <DropdownTrigger>
+                                <Button variant="flat" color="success">
+                                    Menu
+                                </Button>
+                            </DropdownTrigger>
+                            <DropdownMenu aria-label="Static Actions">
+                                <DropdownItem key="home">
+                                    <Link href="/" className="w-full h-full">
+                                        Home
+                                    </Link>
+                                </DropdownItem>
+                                <DropdownItem key="projects">
+                                    <Link
+                                        href="/projects"
+                                        className="w-full h-full"
+                                    >
+                                        Projects
+                                    </Link>
+                                </DropdownItem>
+                                <DropdownItem key="experience">
+                                    <Link
+                                        href="/experience"
+                                        className="w-full h-full"
+                                    >
+                                        Experience
+                                    </Link>
+                                </DropdownItem>
+                                <DropdownItem key="contact">
+                                    <Link
+                                        href="/contact"
+                                        className="w-full h-full"
+                                    >
+                                        Contact
+                                    </Link>
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </Dropdown>
+                    </div>
                 </div>
             </div>
             <Divider />
